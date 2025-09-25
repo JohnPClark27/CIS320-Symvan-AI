6fd20c276d43f06f49af57c868eda4245ff4dcea
JohnPClark27 <161847106+JohnPClark27@users.noreply.github.com>
2025-09-24 20:43:33 -0400

    updated .env

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,23 @@
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
+
+# Load database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 @app.route("/health", methods=["GET"])
 
